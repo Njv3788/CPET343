a8e299a84b6e2571d5589ac1d3f73b0a9b1e1493
Nathaniel Valla (RIT Student) <njv3788@rit.edu>
2022-09-20 10:32:37 -0400

    BCD almotst there

--- Lab3/toPost/counter/src/seven_seg/src/seven_seg.vhd
+++ Lab3/toPost/counter/src/seven_seg/src/seven_seg.vhd
@@ -7,8 +7,10 @@ use ieee.std_logic_1164.all;
 
 entity seven_sig is
   port (
-    binary          : in  std_logic_vector(3 downto 0); 
-    output          : out std_logic_vector(3 downto 0)
+    clk             : in std_logic; 
+    reset           : in std_logic;
+    bcd             : in std_logic_vector(3 downto 0);
+    seven_seg_out   : out std_logic_vector(6 downto 0)
   );  
 end seven_sig;  
 
@@ -18,7 +20,23 @@ begin
   
   process(clk,reset)
   begin
-   
+    if (reset = '1') then 
+      seven_seg_out <= '1111111';
+    elsif (clk'event and clk = '1') then
+      case bcd is 
+      when '0000'=>seven_seg_out <= '1111111';
+      when '0001'=>seven_seg_out <= '1111111';
+      when '0010'=>seven_seg_out <= '1111111';
+      when '0011'=>seven_seg_out <= '1111111';
+      when '0100'=>seven_seg_out <= '1111111';
+      when '0101'=>seven_seg_out <= '1111111';
+      when '0110'=>seven_seg_out <= '1111111';
+      when '0111'=>seven_seg_out <= '1111111';
+      when '1000'=>seven_seg_out <= '1111111';
+      when '1001'=>seven_seg_out <= '1111111';
+      when others  seven_seg_out <= '1111111';
+      end case;
+    end if;
   end process;
   
 end seven_sig;
