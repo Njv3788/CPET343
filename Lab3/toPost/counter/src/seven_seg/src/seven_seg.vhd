30055ec3ee57f07c1227893a459a69be01b98fe6
Nate Valla <77633870+Njv3788@users.noreply.github.com>
2022-09-20 21:29:08 -0400

    main code done just test benchs and other files

--- Lab3/toPost/counter/src/seven_seg/src/seven_seg.vhd
+++ Lab3/toPost/counter/src/seven_seg/src/seven_seg.vhd
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
--- Dr. Kaputa
--- blink led demo
+-- Nathaniel Valla
+-- BCD hex display
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;      
@@ -24,17 +24,17 @@ begin
       seven_seg_out <= '1111111';
     elsif (clk'event and clk = '1') then
       case bcd is 
-      when '0000'=>seven_seg_out <= '1000000';
-      when '0001'=>seven_seg_out <= '1111001';
-      when '0010'=>seven_seg_out <= '0100100';
-      when '0011'=>seven_seg_out <= '0110000';
-      when '0100'=>seven_seg_out <= '0011001';
-      when '0101'=>seven_seg_out <= '0010010';
-      when '0110'=>seven_seg_out <= '0000010';
-      when '0111'=>seven_seg_out <= '1111000';
-      when '1000'=>seven_seg_out <= '0000000';
-      when '1001'=>seven_seg_out <= '0011000';
-      when others   seven_seg_out <= '1111111';
+      when '0000'=>seven_seg_out <= "1000000";
+      when '0001'=>seven_seg_out <= "1111001";
+      when '0010'=>seven_seg_out <= "0100100";
+      when '0011'=>seven_seg_out <= "0110000";
+      when '0100'=>seven_seg_out <= "0011001";
+      when '0101'=>seven_seg_out <= "0010010";
+      when '0110'=>seven_seg_out <= "0000010";
+      when '0111'=>seven_seg_out <= "1111000";
+      when '1000'=>seven_seg_out <= "0000000";
+      when '1001'=>seven_seg_out <= "0011000";
+      when others   seven_seg_out <= "1111111";
       end case;
     end if;
   end process;
--- Lab3/toPost/counter/src/top.vhd
+++ Lab3/toPost/counter/src/top.vhd
@@ -14,73 +14,81 @@ entity top is
 end top;
 
 architecture beh of top is
+  component generic_counter is
+    generic (
+      max_count       : integer range 0 to 100 := 3
+    );
+    port (
+      clk             : in  std_logic; 
+      reset           : in  std_logic;
+      output          : out std_logic
+    );  
+  end component;  
 
-component generic_counter is
-  generic (
-    max_count       : integer range 0 to 100 := 3
-  );
-  port (
-    clk             : in  std_logic; 
-    reset           : in  std_logic;
-    output          : out std_logic
-  );  
-end component;  
+  component generic_adder_beh is
+    generic (
+      bits    : integer := 4
+    );
+    port (
+      a       : in  std_logic_vector(bits-1 downto 0);
+      b       : in  std_logic_vector(bits-1 downto 0);
+      cin     : in  std_logic;
+      sum     : out std_logic_vector(bits-1 downto 0);
+      cout    : out std_logic
+    );
+  end component;  
 
-component generic_adder_beh is
-  generic (
-    bits    : integer := 4
-  );
-  port (
-    a       : in  std_logic_vector(bits-1 downto 0);
-    b       : in  std_logic_vector(bits-1 downto 0);
-    cin     : in  std_logic;
-    sum     : out std_logic_vector(bits-1 downto 0);
-    cout    : out std_logic
-  );
-end component;  
+  component seven_sign is
+    port (
+      clk             : in  std_logic; 
+      reset           : in  std_logic;
+      bcd             : in  std_logic_vector(3 downto 0);
+      seven_seg_out   : out std_logic_vector(6 downto 0)
+    );  
+  end component; 
 
-component seven_sign is
-  port (
-    clk             : in  std_logic; 
-    reset           : in  std_logic;
-    bcd             : in  std_logic_vector(3 downto 0);
-    seven_seg_out   : out std_logic_vector(6 downto 0)
-  );  
-end component; 
-
-signal sum          : std_logic_vector(3 downto 0);
-signal sum_sig      : std_logic_vector(3 downto 0);
-signal enable       : std_logic;
-
+  signal sum          : std_logic_vector(5 downto 0);
+  signal sum_sig      : std_logic_vector(3 downto 0);
+  signal enable       : std_logic;
 begin
+  process(clk,reset)
+    begin
+      if (reset = '1') then 
+        seven_seg_out <= '1111111';
+      elsif (clk'event and clk = '1') then
+         if(enable = '1')
+          sum_sig <= sum(3 downto 0);
+         end if;
+      end if;
+  end process;
 
-counter: generic_counter 
-  generic map (
-    max_count => 50000000
-  )
-  port map(
-    clk       => clk,
-    reset     => reset,
-    output    => enable
-  );
-  
-adder: generic_adder_beh
-  generic map (
-    bits => 4
-  )
-  port map(
-    a        => sum_sig,
-    b        => '0001',
-    cin      => '0000'
-    sum      => sum,
-    cout     =>
-  );         
-  
-bcd: seven_sign
-  port map(
-    clk           => clk,
-    reset         => reset,
-    bcd           => sum_sig,
-    seven_seg_out => output
-  );         
+  counter: generic_counter 
+    generic map (
+      max_count => 50000000
+    )
+    port map(
+      clk       => clk,
+      reset     => reset,
+      output    => enable
+    );
+    
+  adder: generic_adder_beh
+    generic map (
+      bits => 4
+    )
+    port map(
+      a        => sum_sig,
+      b        => "0001",
+      cin      => "0000",
+      sum      => sum(3 downto 0),
+      cout     => sum(4)
+    );         
+    
+  bcd: seven_sign
+    port map(
+      clk           => clk,
+      reset         => reset,
+      bcd           => sum_sig,
+      seven_seg_out => output
+    );         
 end beh;
